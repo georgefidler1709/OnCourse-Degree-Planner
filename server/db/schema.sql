b13a4f6cabfb8b2380164f8895b43ecbceca7f69
Alex Rowell <a.rowell@unsw.edu.au>
2019-10-20 16:22:31 +1100

    db: Added different types of filters and requirements to CourseFilter and CourseRequirements

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,58 +1,119 @@
 create table Degrees (
-    name varchar(100),
-    id integer primary key
+	name varchar(100),
+	id integer primary key
 );
 
 create table DegreeOfferings (
-    year integer,
-    degree_id integer references Degrees(id),
-    primary key (year, degree_id)
+	year integer,
+	degree_id integer references Degrees(id),
+	primary key (year, degree_id)
 );
 
 create table Courses (
-    letter_code char(4),
-    number_code char(4),
+	letter_code char(4),
+	number_code char(4),
 
-    level integer required check (level > 0),
+	level integer required check (level > 0),
 
-    name varchar(100),
+	name varchar(100),
 
-    prereq integer references CourseRequirements(id),
-    coreq integer references CourseRequirements(id),
-    exclusion integer references CourseRequirements(id),
+	prereq integer references CourseRequirements(id),
+	coreq integer references CourseRequirements(id),
+	exclusion integer references CourseRequirements(id),
 
-    id integer primary key,
+	id integer primary key,
 
-    check (level <= 9),
-    unique (letter_code, number_code)
+	check (level <= 9),
+	unique (letter_code, number_code)
 );
 
 create table Sessions (
-    year integer,
-    -- treat summer term as term 0
-    term integer check (term >= 0),
-    check (term <= 3),
-    primary key (year, term)
+	year integer,
+	-- treat summer term as term 0
+	term integer check (term >= 0),
+	check (term <= 3),
+	primary key (year, term)
 );
 
 create table CourseFilters (
-    id integer primary key
+	type_id integer references CourseFilterTypes(id),
+
+	-- Specific Course filter
+	min_mark integer,
+	course_id integer references Courses(id),
+
+	-- Gen Ed filter has no attributes
+
+	-- Field filter
+	field_code char(4),
+
+	-- Free Elective Filter has no attributes
+
+	-- And and Or filters have relationships in CourseFilterHierarchies
+
+	id integer primary key
+);
+
+-- For And and Or filters, the parent is the And/Or and the child is another filter
+create table CourseFilterHierarchies (
+	parent_id integer references CourseFilters(id),
+	child_id integer references CourseFilters(id),
+
+	primary key (parent_id, child_id)
 );
 
 create table CourseRequirements (
-    id integer primary key
+	type_id integer references CourseRequirementTypes(id),
+
+	-- Completed Course requirement
+	min_mark integer,
+	course_id integer references Courses(id),
+
+	-- Current Degree Requirement
+	degree_id integer references Degrees(id),
+
+	-- Year requirement
+	year integer,
+
+	-- UoC Requirement
+	uoc_amount_required integer,
+	uoc_min_level integer,
+	uoc_subject char(4),
+	uoc_course_requirements integer references CourseRequirements,
+
+	-- And and Or requirements have relationships in CourseRequirementHierarchies
+
+	id integer primary key
 );
 
-create table OfferingRequirements (
-    offering_id integer required references DegreeOfferings(id),
-    requirement_id integer required references CourseRequirements(id),
-    uoc_needed integer required check(uoc_needed > 0),
-    id integer primary key
+-- For And and Or requirements, the parent is the And/Or and the child is another requirement
+create table CourseRequirementHierarchies (
+	parent_id integer references CourseRequirements(id),
+	child_id integer references CourseRequirements(id),
+
+	primary key (parent_id, child_id)
+);
+
+create table DegreeOfferingRequirements (
+	offering_id integer required references DegreeOfferings(id),
+	requirement_id integer required references CourseFilters(id),
+	uoc_needed integer required check(uoc_needed > 0),
+	id integer primary key
 );
 
 create table CourseOfferings (
-    course_id integer references Courses(id),
-    session_year integer references Sessions(year),
-    session_term integer references Sessions(term),
-    primary key (course_id, session_year, session_term)
+	course_id integer references Courses(id),
+	session_year integer references Sessions(year),
+	session_term integer references Sessions(term),
+	primary key (course_id, session_year, session_term)
+);
+
+create table CouseRequirementTypes (
+	name varchar(100),
+	id integer primary key
+);
+
+create table CourseFilterTypes (
+	name varchar(100),
+	id integer primary key
 );

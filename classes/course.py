8816da5ba03d97ad68e1c69c941eafd658688075
Alex Rowell <a.rowell@unsw.edu.au>
2019-10-22 19:33:04 +1100

    Add saving of generic course filter and term into the database

--- classes/course.py
+++ classes/course.py
@@ -1,85 +1,100 @@
 """
 COMP4290 Group Project
-Team: On Course
+Team: On course.Course
 Alexander Rowell (z5116848), Eleni Dimitriadis (z5191013), Emily Chen (z5098910)
 George Fidler (z5160384), Kevin Ni (z5025098)
 
 course.py
-Implementation of the Course class, which represents a course, or individual subject.
+Implementation of the course.Course class, which represents a course, or individual subject.
 It contains a course code, course name, UoC value, offered terms, and details
 concerning requirements.
 
 [MORE INFO ABOUT CLASS]
 """
 
+from flask import g
 from typing import List
 
-from courseReq import CourseReq
-from program import Program
+import courseReq
+import program
 
 class Course(object):
 
-    def __init__(self, subject: str, code: int, name: str, units: int, terms: List[Term],
-            prereqs: CourseReq, coreqs: CourseReq, exclusions: CourseReq):
+    def __init__(self, subject: str, code: int, name: str, units: int, terms: List[int],
+            prereqs: courseReq.CourseReq, coreqs: courseReq.CourseReq, exclusions: courseReq.CourseReq):
         # figure out inputs - database or variables?
         # to be assigned:
-        self._subject = subject
-        self._code = code
-        self._name = name
-        self._units = units
-        self._terms = terms
-        self._prereqs = prereqs
-        self._coreqs = coreqs
-        self._exclusions = exclusions
+        self.subject = subject
+        self.code = code
+        self.name = name
+        self.units = units
+        # TODO: decide whether we want to allow different terms for different years
+        self.years = [2020]
+        self.terms = terms
+        self.prereqs = prereqs
+        self.coreqs = coreqs
+        self.exclusions = exclusions
 
-    # returns the SUBJxxxx course code
+    @property
     def courseCode(self):
-        return self._subject + str(self._code)
+        return self.subject + str(self.code)
 
     @property
-    def name(self):
-        return self._name
-        
-    @property
-    def subject(self):
-        return self._subject
-    
-    @property
-    def level(self)
-    return self._code/1000
+    def subjectArea(self):
+        return self.subject
 
     @property
-    def units(self):
-        return self._units
-    
-    @property
-    def terms(self):
-        return self._terms
+    def level(self):
+        return self.code/1000
 
     # Add an offering of this course in a given term
-    def addOffering(self, term: Term) -> None:
-        self._terms.append(term)
+    def addOffering(self, term: int) -> None:
+        self.terms.append(term)
 
     # Possibly need to be able to modify prereqs/coreqs?
     # Later release
 
     # Input: The program of the student trying to take the course, and the term they're taking it in
     # Return: whether the prerequisites have been fulfilled
-    def prereqsFulfilled(self, program: Program, term: int) -> bool:
-        return self._prereqs.fulfilled(program, term, coreq=False)
+    def prereqsFulfilled(self, program: program.Program, term: int) -> bool:
+        return self.prereqs.fulfilled(program, term, coreq=False)
 
     # Input: The program of the student trying to take the course, the term they're taking it in,
     # and any additional courses they are taking that term
     # Return: whether the corequisites have been fulfilled
-    def coreqsFulfilled(self, program: Program, term: int, additionalCourses: List[Course]) -> bool:
-        return self._coreqs.fulfilled(program, term, additionalCourses, coreq=True)
+    def coreqsFulfilled(self, program: program.Program, term: int, additional_courses: List[Course]) -> bool:
+        return self.coreqs.fulfilled(program, term, additional_courses, coreq=True)
 
     # THINK about corequisites - what if prerequisite OR corequisite?
 
     # Input: The program of the student trying to take the course, the term they are taking it in,
     # and any additional courses they are taking that term
     # Return: whether any exclusion courses have been taken
-    def excluded(self, program: Program, term: int, additionalCourses: List[Course]) -> bool:
-        return self._exclusions.fulfilled(program, term, additionalCourses, coreq=True)
+    def excluded(self, program: program.Program, term: int, additional_courses: List[Course]) -> bool:
+        return self.exclusions.fulfilled(program, term, additional_courses, coreq=True)
+
+    # Saves the course in the database
+    # Return: the id of the course
+    def save(self) -> int:
+        prereq_id = self.prereqs.save()
+        coreq_id = self.coreqs.save()
+        exclusions_id = self.exclusions.save()
+
+        # save the course itself
+        g.db.execute('''insert into Courses(letter_code, number_code, level, name, units, prereq,
+        coreq, exclusion) values (?, ?, ?, ?, ?, ?, ?)''',
+        self.subject, self.code, self.level, self.name, self.units, prereq_id, coreq_id,
+        exclusions_id)
+
+        course_id = g.db.lastrowid
+
+        # save the offerings of the course
+        for term in self.terms:
+            term.save()
+
+            g.db.execute('''insert into CourseOfferings(course_id, session_year, session_term)
+                    values(?, ?, ?)''', course_id, term.year, term.term)
+
+        return course_id
 
 

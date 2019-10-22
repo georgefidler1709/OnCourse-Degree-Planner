5ea5f577a975a108ae5cfb0ff4f335c9c57438a1
Kevin <kevin.zihong.ni@gmail.com>
2019-10-23 02:50:12 +1100

    added documentation

--- server/__init__.py
+++ server/__init__.py
@@ -13,6 +13,8 @@ def create_app() -> Flask:
 
     # Populate CORS header (necessary if we are using two webservers)
     def add_cors_headers(response):
+        # TODO (kevin): this is not ideal, we should figure out a way to
+        # selectively exclude our react webserver
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
     app.after_request(add_cors_headers)

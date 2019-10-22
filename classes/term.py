013bf299da6d0a5689cb12f61f50c34fd59680f3
Eleni D <z5191013@unsw.edu.au>
2019-10-22 19:43:41 +1100

    fixed property convention

--- classes/term.py
+++ classes/term.py
@@ -15,53 +15,45 @@ class Term(object):
     # define a new Term object
     # preconditions: (1 <= term <= 3)
     def __init__(self, year: int, term: int):
-        self._year = year
-        self._term = term
-
-    @property
-    def year(self):
-        return self._year
-
-    @property
-    def term(self):
-        return self._term
+        self.year = year
+        self.term = term
 
     # Override comparison functions
     def __lt__(self, other) -> bool: # x < y
-        if self._year < other.year: return True
-        if self._year > other.year: return False
-        if self._term < other.term: return True
+        if self.year < other.year: return True
+        if self.year > other.year: return False
+        if self.term < other.term: return True
         return False
     
     def __le__(self, other) -> bool: # For x <= y
-        if self._year < other.year: return True
-        if self._year > other.year: return False
-        if self._term <= other.term: return True
+        if self.year < other.year: return True
+        if self.year > other.year: return False
+        if self.term <= other.term: return True
         return False
 
     def __eq__(self, other) -> bool: # For x == y
-        if self._year == other.year and self._term == other.term:
+        if self.year == other.year and self.term == other.term:
             return True
         return False
 
     def __ne__(self, other) -> bool: # For x != y OR x <> y
-        if self._year == other.year and self._term == other.term:
+        if self.year == other.year and self.term == other.term:
             return False
         return True
 
     def __gt__(self, other) -> bool: # For x > y
-        if self._year > other.year: return True
-        if self._year < other.year: return False
-        if self._term > other.term: return True
+        if self.year > other.year: return True
+        if self.year < other.year: return False
+        if self.term > other.term: return True
         return False
 
     def __ge__(self, other) -> bool: # For x >= y
-        if self._year > other.year: return True
-        if self._year < other.year: return False
-        if self._term >= other.term: return True
+        if self.year > other.year: return True
+        if self.year < other.year: return False
+        if self.term >= other.term: return True
         return False
 
     # Saves the term in the database
     def save(self) -> None:
-        g.db.execute('insert or ignore into Sessions(year, term) values (?, ?)', self._year,
-                self._term)
+        g.db.execute('insert or ignore into Sessions(year, term) values (?, ?)', self.year,
+                self.term)

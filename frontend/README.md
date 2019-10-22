46433e39de157e3be9086d32b6b73898e2c2a820
Kevin <kevin.zihong.ni@gmail.com>
2019-10-23 02:50:12 +1100

    added documentation

--- frontend/README.md
+++ frontend/README.md
@@ -1,9 +1,14 @@
 Made using [Create-React-App](https://github.com/facebook/create-react-app).
 
-### `npm start`
-Runs the app in the development mode.<br />
+# Running in dev
+`npm start` to run the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
 Dependencies:
 - bootstrap
 - react-bootstrap
+
+# Building for prod 
+
+run `npm run build` to compile react files
+run `serve -l 80 -s build` to run the webserver on [http://localhost:80](http://localhost:80) 
--- server/__init__.py
+++ server/__init__.py
@@ -3,10 +3,6 @@ from flask import Flask;
 from . import degrees;
 from . import db;
 
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    return response
-
 def create_app() -> Flask:
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
@@ -14,6 +10,13 @@ def create_app() -> Flask:
     with app.app_context():
     	db.init_app(app)
     	app.register_blueprint(degrees.degrees_bp)
+
+    # Populate CORS header (necessary if we are using two webservers)
+    def add_cors_headers(response):
+        # TODO (kevin): this is not ideal, we should figure out a way to
+        # selectively exclude our react webserver
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
     app.after_request(add_cors_headers)
 
     return app

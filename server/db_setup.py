5cf224a226474c9c4ba86ce7d83af82510ad8474
Emily <emily.chen@unsw.edu.au>
2019-10-27 13:52:12 +1100

    __init__.py in db folder does not break things

--- server/db_setup.py
+++ server/db_setup.py
@@ -9,7 +9,7 @@ from flask import current_app, g, Flask
 from flask.cli import with_appcontext
 import pandas
 
-# import input_data
+# from server.db import input_data
 
 def get_db() -> sqlite3.Connection:
     if 'db' not in g:

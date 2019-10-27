6be3639add86299b20129d500bb8e3ce31f374e2
Emily <emily.chen@unsw.edu.au>
2019-10-27 12:12:53 +1100

    actually do not need separate input_courses, put in create_db.sh

--- server/db/create_db.sh
+++ server/db/create_db.sh
@@ -4,3 +4,14 @@ db='university.db'
 sqlite3 $db < schema.sql
 sqlite3 $db < setup_enums.sql
 
+sqlite3 $db < data.sql
+
+# will have an error reading line 1 as that's the header line
+sqlite3 'university.db' <<EOF
+.mode csv
+.import courses.csv Courses
+update Courses set prereq = NULL where prereq = "";
+update Courses set coreq = NULL where coreq = "";
+update Courses set exclusion = NULL where exclusion = "";
+update Courses set equivalent = NULL where equivalent = "";
+EOF

0230b4b6452b7969cee4faefe4f34071e6d52505
Kevin <kevin.zihong.ni@gmail.com>
2019-11-07 01:10:03 +1100

    search bar

--- frontend/src/Types.tsx
+++ frontend/src/Types.tsx
@@ -1,2 +1,8 @@
+import {SimpleDegree} from './Api'
+
 export type Position = "auto-start" | "auto" | "auto-end" | "top-start" | "top" | "top-end" | "right-start" | "right" | "right-end" | "bottom-end" | "bottom" | "bottom-start" | "left-end" | "left" | "left-start"
   
+export interface SearchResult {
+  degree: SimpleDegree 
+  text: JSX.Element 
+}
--- frontend/src/__components__/degree_search/Search.tsx
+++ frontend/src/__components__/degree_search/Search.tsx
@@ -1,31 +1,67 @@
 import React, { Component, ChangeEvent } from 'react'
 import Suggestions from './Suggestions'
 import {API_ADDRESS} from '../../Constants'
-import {SimpleDegrees} from '../../Api'
+import {SimpleDegrees, SimpleDegree} from '../../Api'
+import {SearchResult} from '../../Types'
 
-class Search extends Component<{}, {query: string; degrees: SimpleDegrees}> {
+interface SearchState {
+  searchResults : Array<SearchResult>;
+  degrees: SimpleDegrees
+  oldQuery: string
+}
+
+class Search extends Component<{}, SearchState> {
   constructor(props: {}) {
     super(props)
     this.state = {
-      query: '',
+      searchResults: [],
       degrees: [],
+      oldQuery: '',
     }
-    this.handleInputChange = this.handleInputChange.bind(this)
-  }
-
-  getDegrees(): void {
     fetch(API_ADDRESS + '/degrees.json')
     .then(response => response.json())
     .then(degrees => {
       this.setState({ degrees })
     })
+
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
+
   handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
-    this.setState({
-      query: event.target.value
-    })
-    this.getDegrees()
+    let query = event.target.value.toLowerCase();
+    let searchResults: Array<SearchResult> = [];
+    console.log(event);
+
+    function processDegree(degree: SimpleDegree) {
+      let index = degree.name.toLowerCase().indexOf(query);
+      if (index !== -1) {
+        let begin = degree.name.substring(0, index);
+        let mid = degree.name.substring(index, index + query.length);
+        let end = degree.name.substring(index + query.length)
+        searchResults.push({
+          degree,
+          text: <>{begin}<u>{mid}</u>{end}</>
+        })
+      }
+      else if (degree.id.includes(query)) {
+        searchResults.push({degree, text: <>{degree.name}</>})
+      }
+    }
+
+    if (query.length !== 0) {
+      if (this.state.oldQuery.length !== 0 && (query.startsWith(this.state.oldQuery) || query.endsWith(this.state.oldQuery))) {
+        for (let result of this.state.searchResults) {
+          processDegree(result.degree);
+        }
+      }
+      else {
+        for (let degree of this.state.degrees) {
+          processDegree(degree);
+        }
+      }
+    }
+    this.setState({ searchResults, oldQuery: query });
   }
 
   render() {
@@ -35,14 +71,13 @@ class Search extends Component<{}, {query: string; degrees: SimpleDegrees}> {
           <input
             className="search-bar"
             placeholder="Search for your degree..."
-            value={this.state.query}
+            //value={this.state.query}
             onChange={this.handleInputChange}
           />
         </form>
       {
-        this.state.degrees.length > 0 && 
-        this.state.query.length > 0 &&
-        <Suggestions degrees={this.state.degrees} />
+        this.state.searchResults.length > 0 &&
+        <Suggestions degrees={this.state.searchResults} />
         }
       </div>
       )

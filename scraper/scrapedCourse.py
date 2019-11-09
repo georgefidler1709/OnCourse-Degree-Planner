12c8d9fd57e06e2949ad7b7a751af64895bd30c2
Alex Rowell <a.rowell@unsw.edu.au>
2019-11-09 21:51:42 +1100

    added class for scraped course

---
+++ scraper/scrapedCourse.py
@@ -0,0 +1,35 @@
+"""
+COMP4290 Group Project
+Team: On course.Course
+Alexander Rowell (z5116848), Eleni Dimitriadis (z5191013), Emily Chen (z5098910)
+George Fidler (z5160384), Kevin Ni (z5025098)
+
+scrapedCourse.py
+A class to contain information about a course, scraped from the handbook
+
+[MORE INFO ABOUT CLASS]
+"""
+
+
+from typing import List
+
+class ScrapedCourse(object):
+    def __init__(self,
+            year: int,
+            overview: str,
+            equivalents: List[str],
+            exlusions: List[str],
+            requirements: str,
+            faculty: str,
+            school: str,
+            study_level: str,
+            terms: str):
+        self.year = year
+        self.overview = overview
+        self.equivalents = equivalents
+        self.exclusions = exclusions
+        self.requirements = requirements
+        self.faculty = faculty
+        self.school = school
+        self.study_level = study_level
+        self.terms = terms

4c97de02dac1370d0fef7d0b30a815b6595589af
Eleni D <z5191013@unsw.edu.au>
2019-11-12 20:11:22 +1100

    term parser complete

--- scraper/scrapedCourse.py
+++ scraper/scrapedCourse.py
@@ -37,3 +37,34 @@ class ScrapedCourse(object):
         self.study_level = study_level
         self.terms = terms
         self.units = units
+
+    # Convert to a course object
+    def to_course(self):
+        subject = self.code[0..3]
+        code  = self.code[4..7]
+        name = ""
+        units = self.units
+        terms = self.parse_terms()
+        faculty = self.faculty
+        prereqs = self.parse_prereqs()
+        coreqs = self.parse_coreqs()
+        exclusions = self.parse_exclusions()
+        equivalents = self.parse_equivalents()
+
+        return course.Course(subject, code, name, units, terms, faculty,
+                    prereqs, coreqs, exclusions, equivalents)
+
+
+    # Parse the string to return the list of term offerings
+    def parse_terms(self) -> List['term.Term']:
+        terms: List['term.Term'] = []
+        for i in range(1, 4):
+            if str(i) in self.terms:
+                terms.append(term.Term(self.year, i))
+        return terms
+        # TODO summer terms
+
+    def parse_equivalents(self):
+        for c in self.equivalents:
+            code = get_course_code(c)
+            

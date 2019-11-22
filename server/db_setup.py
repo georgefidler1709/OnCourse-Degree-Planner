f0c6fb53a346082156752382d570043559d273a7
Emily Chen <emily.chen@unsw.edu.au>
2019-11-22 14:19:38 +1100

    put 7001 and 7002 in db as Degree, changed quotes to single in the input_data.py file

--- server/db_setup.py
+++ server/db_setup.py
@@ -79,6 +79,8 @@ def do_add_to_db() -> None:
 
     db_path = current_app.config['DATABASE']
 
+    input_data.insert_degrees_with_no_offerings()
+
     # input Computer Science 3778 COMPA1 course requirements
     # In case we missed requirements for some courses
     input_data.compsci_course_reqs(db_path)
